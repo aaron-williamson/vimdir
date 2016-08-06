fa80b0f30fb3a379a4fa85ac068aa1d1fdae9db1
Aaron Williamson <aarowill@cisco.com>
2016-08-06 14:48:43 -0600

    Made setup script work from different directories

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+current_dir=$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd )
+
+echo $current_dir
 # Download vundle
-mkdir -p bundle
-git clone https://github.com/VundleVim/Vundle.vim bundle/Vundle.vim
+mkdir -p $current_dir/bundle
+git clone https://github.com/VundleVim/Vundle.vim $current_dir/bundle/Vundle.vim
 
 # Install plugins
-vim -u plugins.vim +PluginInstall +qall
+vim -u $current_dir/plugins.vim +PluginInstall +qall
