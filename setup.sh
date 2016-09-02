784b5bb759b1245b74862a70b5fe1e6592b1106c
Aaron Williamson <aarowill@cisco.com>
2016-09-02 15:46:35 -0600

    Updated setup script to barf less

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@ set -e
 
 current_dir=$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd )
 
-echo $current_dir
-# Download vundle
-mkdir -p $current_dir/bundle
-git clone https://github.com/VundleVim/Vundle.vim $current_dir/bundle/Vundle.vim
+# Download vundle if there isn't already a vundle folder
+if [ ! -d $current_dir/bundle/Vundle.vim ]; then
+  mkdir -p $current_dir/bundle
+  git clone https://github.com/VundleVim/Vundle.vim $current_dir/bundle/Vundle.vim
+fi
 
-# Install plugins
+# Install plugins, may run into problems if the vundle.vim folder
+# was created but vundle wasn't installed
 vim -u $current_dir/plugins.vim +PluginInstall +qall
