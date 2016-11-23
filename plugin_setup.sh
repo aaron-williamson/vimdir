a47b04ca275c3299e6e6eba6d7715e6ae925648a
Aaron Williamson <guitarfanman@gmail.com>
2016-11-22 21:13:45 -0700

    Update plugin setup script for vim-plug

--- plugin_setup.sh
+++ plugin_setup.sh
@@ -4,22 +4,22 @@ set -e
 
 current_dir=$( cd "$( dirname ${BASH_SOURCE[0]:-$0} )" && pwd )
 
-# Download vundle if there isn't already a vundle folder
-if [[ ! -d $current_dir/bundle/Vundle.vim ]]; then
-  echo Installing vundle
-  mkdir -p $current_dir/bundle
-  git clone https://github.com/VundleVim/Vundle.vim $current_dir/bundle/Vundle.vim
+# Download vim-plug if it isn't already downloaded
+if [[ -f $current_dir/autoload/plug.vim ]]; then
+  echo Existing vim-plug detected, skipping download
 else
-  echo Vundle directory detected, skipping vundle install
+  echo Downloading vim-plug...
+  curl -fLo $current_dir/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Install plugins, may run into problems if the vundle.vim folder
 # was created but vundle wasn't installed ?
 echo Installing plugins...
 if hash nvim 2>/dev/null; then
-  nvim +PluginInstall +qall
+  nvim +PlugInstall +qall
 else
-  vim +PluginInstall +qall
+  vim +PlugInstall +qall
 fi
 
 echo Vim plugin setup complete
